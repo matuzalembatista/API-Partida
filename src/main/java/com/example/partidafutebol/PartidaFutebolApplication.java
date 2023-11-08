440e9ad7b4f23b08e29613cf33232593500e51c1
matuzbatista <matuzalem_pereira@hotmail.com>
2023-11-08 15:34:28 -0300

    Criando API para partidas de futebol

---
+++ src/main/java/com/example/partidafutebol/PartidaFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.example.partidafutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidaFutebolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartidaFutebolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/partidafutebol/PartidaFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.partidafutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidaFutebolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
